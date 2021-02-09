eea00f3ed615b7f6e1d86edd313a316f45006d94
Vianney Gall <vianneygall@gmail.com>
2021-02-08 22:06:39 -0500

    Switch appear on screean!!! Brap brap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      switches: "inconnu"
+    };
+
+  }
+
+  updateSwitches() {
+    const http = new XMLHttpRequest();
+    const url = 'http://192.168.1.10/cmd/sws';
+
+    http.open("GET", url);
+    http.send();
+    http.onload = () => this.setState({switches: http.responseText});
+
+  }
+
+  componentDidMount() {
+    this.interval = setInterval( () => this.updateSwitches(), 10);
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render(){
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>Hello!</p>
+
+            <p>L'état des interrupteurs est : </p>
+            <b><p id="switches">{this.state.switches}</p></b>
+
+          </header>
+        </div>
+    );
+  }
 }
+
 
 export default App;
